d025ddd284000027d810cf78fa215c358ac2ee78
hjfenghj <72334253+hjfenghj@users.noreply.github.com>
2024-03-16 22:08:26 +0800

    add build sh

---
+++ convert_mini_scenes.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# This script is an example usage of `convert_to_mcap.py` to convert the nuScenes mini-v1.0 dataset to MCAP.
+
+docker build -t uto_nuscenes2mcao_tools .
+mkdir -p output
+mkdir -p data
+

5cbfb0f929720ade5a3497ae0a343160b2a298ef
hjfenghj <hjfenghj5182163.com>
2024-03-17 18:32:18 +0800

    fix build.sh and run.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 # This script is an example usage of `convert_to_mcap.py` to convert the nuScenes mini-v1.0 dataset to MCAP.
 
-docker build -t uto_nuscenes2mcao_tools .
+docker build -t uto_nuscenes2mcap_tools .
 mkdir -p output
 mkdir -p data
 
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-sudo docker run -ti -v $(pwd)/data_link:/work/data -v $(pwd)/output:/work/output  uto_nusc_mcap_tools /bin/bash
+sudo docker run -ti -v $(pwd)/data_link:/work/data -v $(pwd)/output:/work/output  uto_nuscenes2mcap_tools /bin/bash
 # data_link为原始数据所在路径，可以是软连接
